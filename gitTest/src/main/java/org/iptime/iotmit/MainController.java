0e2001e16e8df05752005753314d7fcb1deb79d7
Jin Junsu <junsu1208@naver.com>
2020-12-30 11:51:09 +0900

    글자 수 입력 화면 완성

---
+++ gitTest/src/main/java/org/iptime/iotmit/MainController.java
@@ -0,0 +1,14 @@
+package org.iptime.iotmit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/strcount")
+	public void count() {
+		// "/strcount.jsp"∑Œ ¿Ãµø
+	}
+
+}
---
+++ gitTest/src/test/java/CodeTest/TestStrCount.java
@@ -0,0 +1,13 @@
+package CodeTest;
+
+import org.junit.Test;
+
+public class TestStrCount {
+	
+	@Test
+	public void countTest() {
+		String str="이 글자는 몇 글자일까요?";
+		System.out.println(str.length());
+	}
+	
+}
